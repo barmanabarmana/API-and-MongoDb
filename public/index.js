93e50dc31e0805cd24ea2b64351aa4fecb70c494
barmanabarmana <barmana2003@gmail.com>
2022-06-26 14:22:08 +0200

    Cookies for auth

--- public/index.js
+++ public/index.js
@@ -22,10 +22,47 @@ const connect = mongoose.connect(url);
 const url1 = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 app.use(logger('dev'));
+app.use(cookieParser('12345-67890-09876-54321'));
 app.use(express.json());
 app.use(express.urlencoded({ extended:false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+function auth (req, res, next) {
+   if(!req.signedCookies.user) {
+      let authHeader = req.headers.authorization;
+      if (!authHeader) {
+         let err = new Error('You are not authenticated!');
+         res.setHeader('WWW-Authenticate', 'Basic');
+         err.status = 401;
+         next(err);
+         return;
+      }
+
+      let auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+      let user = auth[0];
+      let pass = auth[1];
+      if (user === 'admin' && pass === 'password') {
+         res.cookie('user', 'admin', {signed: true});
+         next(); // authorized
+      } else {
+         let err = new Error('Wrong login or password.Try again!');
+         res.setHeader('WWW-Authenticate', 'Basic');
+         err.status = 401;
+         next(err);
+      }
+   }else {
+      if(req.signedCookies.user === 'admin') {
+         next();
+      }else {
+         let err = new Error('You are not authenticated!');
+         err.status = 401;
+         next(err);
+      }
+   }
+}
+
+app.use(auth);
 
 
 MongoClient.connect(url1, (err, client) => {
