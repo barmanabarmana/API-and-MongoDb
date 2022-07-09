c0dffa87e8c8a48fe39378c193574bd08e84b519
barmanabarmana <barmana2003@gmail.com>
2022-07-09 22:44:56 +0200

    Stopped tracking config.js  File

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "secretKey": "12345-67890-09876-54321",
-    "mongoUrl": "mongodb://localhost:27017/conFusion",
-    "facebook": {
-        clientId:'789839285509666',
-        clientSecret: '40cb47d134303cce808094d2ee7c7bf5'
-    }
-}
-
